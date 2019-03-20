0ae618136e88c4a8de416bc8a8952edbc17cfbff
Kimmy <kenpusney@gmail.com>
2019-03-20 14:22:08 +0800

    [kimmy] Add Customer query

--- src/main/java/graphql/example/resolvers/QueryResolver.java
+++ src/main/java/graphql/example/resolvers/QueryResolver.java
@@ -1,13 +1,23 @@
 package graphql.example.resolvers;
 
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+import graphql.example.constraints.Customer;
+import graphql.example.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 
 @Component
 public class QueryResolver implements GraphQLQueryResolver {
 
+    @Autowired
+    private CustomerRepository customerRepository;
+
     String message() {
         return "Hello world!";
+    }
+
+    Customer customer(String name) {
+        return customerRepository.queryCustomer(name);
     }
 }
