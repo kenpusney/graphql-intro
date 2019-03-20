ff1e5b7c2b232a0e9e8367313a1cd0dd9508e051
Kimmy <kenpusney@gmail.com>
2019-03-20 14:34:45 +0800

    [kimmy] Add Product into Cart: schema
    [kimmy] Add Product into Cart: schema
    
    [kimmy] Add Product into Cart: schema

---
+++ src/main/java/graphql/example/resolvers/MutationResolver.java
@@ -0,0 +1,8 @@
+package graphql.example.resolvers;
+
+import com.coxautodev.graphql.tools.GraphQLMutationResolver;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MutationResolver implements GraphQLMutationResolver {
+}
